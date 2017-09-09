347fcdd57469bf5ec8e8ba2cb48e4de9e561f931
Muhammad Yusuf <muhammadyusuf3799@gmail.com>
2017-09-09 09:28:09 -0400

    Update Slingshot.java

--- Slingshot.java
+++ Slingshot.java
@@ -1,45 +1,31 @@
 import java.awt.*;
-import acm.program.*;
-import acm.graphics.*;
-import java.awt.Color;
-import java.awt.event.*;
-import java.util.*;
-import java.applet.AudioClip;
-import acm.util.MediaTools;
+import javax.swing.*;
 
-public class Slingshot extends JLabel
-{
-    private int vx;
-    private int power;
+public class Slingshot extends JLabel {
+	private int power;
 
-    public Slingshot(ImageIcon i, int width, int height)
-    {
-        super(i);
-        setPreferredSize(new Dimension(width, height));
-        setVisible(False);
-    }
+	public Slingshot(ImageIcon i, int width, int height) {
+		super(i);
+		setPreferredSize(new Dimension(width, height));
+		setVisible(false);
+	}
 
-    public void move(int vx)
-    {
-        setX(getX()+vx)
-    }
-    
-    public void setPower(int p)
-    {
-        power = p;
-    }
-    
-    public int getPower()
-    {
-        return power;
-    }
-    
-    public Projectile shoot()
-    {
-        Projectile p = new Projectile (power, getX());
-        setPower(0);
-        return p;
-    }
+	public void move(int vx) {
+		setBounds(getX() + vx, getY(), getWidth(), getHeight());
+	}
 
+	public void setPower(int p) {
+		power = p;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public Projectile shoot() {
+		Projectile p = new Projectile(power, getX());
+		setPower(0);
+		return p;
+	}
 
 }

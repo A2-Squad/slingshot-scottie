b00b34f2ec7c693839da12232be66939a6fd05d9
Muhammad Yusuf <muhammadyusuf3799@gmail.com>
2017-09-09 09:28:45 -0400

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -1,25 +1,16 @@
 import java.awt.*;
-import acm.program.*;
-import acm.graphics.*;
-import java.awt.Color;
-import java.awt.event.*;
-import java.util.*;
-import java.applet.AudioClip;
-import acm.util.MediaTools;
+import javax.swing.*;
 
-public class Projectile extends JLabel
-{
-    private int speed;
-    
-    public Projectile(ImageIcon i, int width, int height)
-    {
-        super(i);
-        setPreferredSize(new Dimension(width, height));
-    }
-    
-    public void move()
-    {
-        setY(getY()-speed);
-    }
+public class Projectile extends JLabel {
+	private int speed;
+
+	public Projectile(ImageIcon i, int width, int height) {
+		super(i);
+		setPreferredSize(new Dimension(width, height));
+	}
+
+	public void move() {
+		setBounds(getX(), getY() - speed, getWidth(), getHeight());
+	}
 
 }
